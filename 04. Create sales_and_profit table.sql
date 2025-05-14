40431a549708bace34b6c4df8c53b4311e0a7ef3
TT-Henry <hoangphh1905@gmail.com>
2025-05-14 15:24:59 +0700

    Adventure Works

--- 04. Create sales_and_profit table.sql
+++ 04. Create sales_and_profit table.sql
@@ -2,7 +2,7 @@ SELECT
 COALESCE(consumer.order_date, retailer.order_date) AS 'date',
 consumer.product_key, COALESCE(consumer.sales_territory_key, retailer.territory_id) AS region,
 consumer.sales AS consumer_sales, retailer.sales_amount AS retailer_sales, 
-consumer.sales - consumer.cost AS consumer_profit, retailer.sub_total AS retailer_profit
+(consumer.sales - consumer.cost) AS consumer_profit, retailer.sub_total AS retailer_profit
 INTO sales_and_profit
 FROM sales_consumers3 AS consumer
 FULL OUTER JOIN sales_retailers3 AS retailer
